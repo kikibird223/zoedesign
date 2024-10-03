14b4f9649dce65a90f40b0f926fdf133d617e64c
kikibird223 <setusiyo3@gmail.com>
2024-10-03 22:37:08 +0800

    [add]sql

---
+++ sql/mywebsite.sql
@@ -0,0 +1,152 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2024-10-01 23:14:27
+-- 伺服器版本： 10.4.32-MariaDB
+-- PHP 版本： 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `mywebsite`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `categories`
+--
+
+CREATE TABLE `categories` (
+  `category_id` int(11) NOT NULL,
+  `category_image_url` varchar(50) DEFAULT NULL,
+  `category_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- 傾印資料表的資料 `categories`
+--
+
+INSERT INTO `categories` (`category_id`, `category_image_url`, `category_name`) VALUES
+(2, '客製.png', '客製'),
+(3, '設計.png', '設計'),
+(4, '課程.png', '課程'),
+(5, '優惠.png', '優惠'),
+(6, '商品.png', '商品');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `login`
+--
+
+CREATE TABLE `login` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- 傾印資料表的資料 `login`
+--
+
+INSERT INTO `login` (`id`, `username`, `password`, `created_at`) VALUES
+(1, 'kiki223', '123456', '2024-09-18 02:13:23');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `products`
+--
+
+CREATE TABLE `products` (
+  `product_id` int(11) NOT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `product_title` varchar(255) NOT NULL,
+  `product_price` decimal(10,2) NOT NULL,
+  `product_image_url` varchar(255) DEFAULT NULL,
+  `product_iframe_url` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- 傾印資料表的資料 `products`
+--
+
+INSERT INTO `products` (`product_id`, `category_id`, `product_title`, `product_price`, `product_image_url`, `product_iframe_url`) VALUES
+(1, 3, '3D機構+渲染', 2000.00, NULL, 'https://sketchfab.com/models/6a67b2607d6443e3b08830f9feceea98/embed'),
+(2, 2, '壓克力商品設計', 1000.00, '壓克力商品設計.jpg', NULL),
+(3, 4, '造型黏土課程', 200.00, '7月_小小綿羊.jpg', NULL),
+(4, 5, '3D 建模', 2000.00, NULL, 'https://sketchfab.com/models/fb92021ea64c465d922d0062ce26e960/embed'),
+(5, 3, 'LOVE LETTER<br>恩典卡盒', 200.00, 'love-letter.gif', NULL);
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- 資料表索引 `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- 資料表索引 `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`product_id`),
+  ADD KEY `category_id` (`category_id`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `login`
+--
+ALTER TABLE `login`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `products`
+--
+ALTER TABLE `products`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- 已傾印資料表的限制式
+--
+
+--
+-- 資料表的限制式 `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
